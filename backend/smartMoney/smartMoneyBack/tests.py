eb29898d6aac7a7e487103facac5e6d1b135180d
frandegrandis <frandegrandis@gmail.com>
2021-08-27 18:09:46 -0300

    test de prueba

--- backend/smartMoney/smartMoneyBack/tests.py
+++ backend/smartMoney/smartMoneyBack/tests.py
@@ -2,27 +2,13 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 
-class TestExample(TestCase):
-    """
-    This class contains tests that convert measurements from one
-    unit of measurement to another.
-    """
-    def setUp(self):
-        """
-        This method runs before the execution of each test case.
-        """
-        #self.client = Client()
-        #self.url = reverse("le:convert")
-        return
+from smartMoneyBack.models import Page
 
-    def test_centimetre_to_metre_conversion(self):
-        """
-        Tests conversion of centimetre measurements to metre.
-        """
-        data = {
-            "input_unit": "centimetre",
-            "output_unit": "metre",
-            "input_value": round(8096.894, 3)
-        }
-        #response = self.client.get(self.url, data)
-        #self.assertContains(data, data)
+
+class WebsiteTests(TestCase):
+    def test_page_is_created_successfully(self):
+        page = Page(
+            name='Home',
+            slug='home'
+        )
+        page.save()
