8187ec5e0813d4864508b04dd9d2e5855755154b
frandegrandis <frandegrandis@gmail.com>
2021-09-08 18:41:26 -0300

    Revert "fix"
    This reverts commit 50a0eaa968fc59fea8dc4e5edb3a36ead17aab45.

--- backend/smartMoney_project/smartMoney_project/settings.py
+++ backend/smartMoney_project/smartMoney_project/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
+SECRET_KEY = 'django-insecure-wy)4&s3$%a&qc0!igmxqz7+ty^a)a0*%njt)!vpe!m6&r)#(-g'
 
-SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
