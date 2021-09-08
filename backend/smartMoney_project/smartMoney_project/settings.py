50a0eaa968fc59fea8dc4e5edb3a36ead17aab45
frandegrandis <frandegrandis@gmail.com>
2021-09-08 18:19:56 -0300

    fix

--- backend/smartMoney_project/smartMoney_project/settings.py
+++ backend/smartMoney_project/smartMoney_project/settings.py
@@ -23,8 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wy)4&s3$%a&qc0!igmxqz7+ty^a)a0*%njt)!vpe!m6&r)#(-g'
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
