ecaa81a76222b35f63b63c20995c820ce7419d0b
frandegrandis <frandegrandis@gmail.com>
2021-09-23 02:48:49 -0300

    Landing forms changed

--- frontend/smartmoney/src/components/landing/Register.js
+++ frontend/smartmoney/src/components/landing/Register.js
@@ -7,6 +7,12 @@ import { useMediaQuery } from 'react-responsive';
 import RequiredField from '../RequiredField/requiredField';
 import isValidPassword from '../../functions/passwordFormatValidation';
 
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+
+
 const Register = ({setLayoutLogin}) => {
 
     const isMobileDevice = useMediaQuery({
@@ -65,38 +71,65 @@ const Register = ({setLayoutLogin}) => {
     return(
         <div className="formContainer" name="Register">     
             <form className="form">
-                <p className="label" >Email</p>
-                <input style={isMobileDevice ? (userInvalid ? mobilStyles.inputEmpty : mobilStyles.input) : (userInvalid ? webStyles.inputEmpty : webStyles.input)} type="text" value={mail} onChange={e => setMail(e.target.value)} onFocus={()=>setUserInvalid(false)} onBlur={()=>isValidEmail(mail,setUserInvalid)}/>
-                {userInvalid&&
-                    <p className="invalidCredentials" style={isMobileDevice ? mobilStyles.invalidCredentials : webStyles.invalidCredentials}>Wrong email</p>
-                }
-                <p className="label">Name</p>
-                <input style={isMobileDevice ? (nameEmpty ? mobilStyles.inputEmpty : mobilStyles.input) : (nameEmpty ? webStyles.inputEmpty : webStyles.input)} type="text" value={name} onChange={e => setName(e.target.value)} onFocus={()=>setNameEmpty(false)} onBlur={()=>isEmpty(name,setNameEmpty)}/>
-                {nameEmpty&&
-                    <RequiredField/>
-                }
-                <p className="label">Last name</p>
-                <input style={isMobileDevice ? (surnameEmpty ? mobilStyles.inputEmpty : mobilStyles.input) : (surnameEmpty ? webStyles.inputEmpty : webStyles.input)} type="text" value={surname} onChange={e => setSurname(e.target.value)} onFocus={()=>setSurnameEmpty(false)} onBlur={()=>isEmpty(surname,setSurnameEmpty)}/>
-                {surnameEmpty&&
-                    <RequiredField/>
-                }
-                <p className="label">Password</p>
-                <input style={isMobileDevice ? (invalidPassword ? mobilStyles.inputEmpty : mobilStyles.input) : (invalidPassword ? webStyles.inputEmpty : webStyles.input)} type="password" value={password} onChange={e => setPassword(e.target.value)} onFocus={()=>setInvalidPassword(false)} onBlur={()=>setInvalidPassword(!isValidPassword(password))}/>
-                {invalidPassword&&
-                    <p className="invalidCredentials" style={isMobileDevice ? mobilStyles.invalidCredentials : webStyles.invalidCredentials}>Password must have al least 8 caracters, 1 number, 1 uppercase and 1 lowercase</p>
-                }
-                <input  
+                <TextField 
+                label = "Email" variant = 'outlined' 
+                margin="dense"
+                required
+                size = 'small'
+                error = {userInvalid}
+                helperText={userInvalid ? 'Wrong email' : ' '}
+                //style={isMobileDevice ? (userInvalid ? mobilStyles.inputEmpty : mobilStyles.input) : (userInvalid ? webStyles.inputEmpty : webStyles.input)} 
+                type="text" 
+                value={mail} 
+                onChange={e => setMail(e.target.value)}
+                 onFocus={()=>setUserInvalid(false)} 
+                 onBlur={()=>isValidEmail(mail,setUserInvalid)}/>
+                <TextField
+                label = "Name" variant = 'outlined' 
+                margin="dense"
+                required
+                size = 'small'
+                error = {nameEmpty}
+                helperText={nameEmpty ? 'This field is required' : ' '}
+                //style={isMobileDevice ? (nameEmpty ? mobilStyles.inputEmpty : mobilStyles.input) : (nameEmpty ? webStyles.inputEmpty : webStyles.input)} 
+                type="text" 
+                value={name} 
+                onChange={e => setName(e.target.value)}
+                onFocus={()=>setNameEmpty(false)} 
+                onBlur={()=>isEmpty(name,setNameEmpty)}/>
+                <TextField 
+                label = "Last name" variant = 'outlined' 
+                margin="dense"
+                required
+                size = 'small'
+                error = {surnameEmpty}
+                helperText={surnameEmpty ? 'This field is required' : ' '}
+                //style={isMobileDevice ? (surnameEmpty ? mobilStyles.inputEmpty : mobilStyles.input) : (surnameEmpty ? webStyles.inputEmpty : webStyles.input)} 
+                type="text" value={surname} onChange={e => setSurname(e.target.value)} onFocus={()=>setSurnameEmpty(false)} onBlur={()=>isEmpty(surname,setSurnameEmpty)}/>
+                <TextField
+                label = "Password" variant = 'outlined' 
+                margin="dense"
+                required
+                size = 'small'
+                error = {invalidPassword}
+                helperText={invalidPassword ? 'Password must have al least 8 caracters, 1 number, 1 uppercase and 1 lowercase' : ' '}
+                //style={isMobileDevice ? (invalidPassword ? mobilStyles.inputEmpty : mobilStyles.input) : (invalidPassword ? webStyles.inputEmpty : webStyles.input)} 
+                type="password" value={password} onChange={e => setPassword(e.target.value)} onFocus={()=>setInvalidPassword(false)} onBlur={()=>setInvalidPassword(!isValidPassword(password))}/>
+                <Button  
+                    variant = "contained"
                     className = "button1"
                     type="button" 
                     onClick={registerSubmit} 
-                    value="Register" 
-                    disabled={userInvalid||nameEmpty||surnameEmpty||invalidPassword}/>
+                    disabled={userInvalid||nameEmpty||surnameEmpty||invalidPassword}>
+                    Register
+                  </Button>
                 <div className="line"></div>
-                <input 
-                    className = "button2"
+                <Button 
+                    variant="outlined"
                     type="button" 
-                    onClick={setLayoutLogin} 
-                    value="Login" />
+                    onClick={setLayoutLogin}>
+                    Login
+                </Button>
             </form>
           </div> 
     )
--- frontend/smartmoney/src/components/landing/mobilStyles.js
+++ frontend/smartmoney/src/components/landing/mobilStyles.js
@@ -1,16 +1,23 @@
 const mobilStyles = {
-    forgotPassword:{backgroundColor:"#D7D7D6",borderStyle:'none', color:'blue', textDecorationLine: 'underline', textAlign:'left', fontSize:10,webkitAppearance: 'none',},
+
+    forgotPassword:{
+        backgroundColor:"#D7D7D6",
+        borderStyle:'none', color:'blue', 
+        textDecorationLine: 'underline',
+         textAlign:'left', 
+         fontSize:6,
+         webkitAppearance: 'none',},
     input: {
         backgroundColor: "#D7D7D6",
         borderRadius: 5,
         padding: 3,
-        fontSize:12,
+        fontSize:10,
     },
     inputEmpty: {
         backgroundColor: "#D7D7D6",
         borderRadius: 5,
         padding: 3,
-        fontSize:12,
+        fontSize:10,
         borderColor:'red',
     },
 }
