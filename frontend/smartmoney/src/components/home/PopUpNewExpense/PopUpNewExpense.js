9bcaa2ba9f98bd3dcf920762b75a05abde770b91
Fs <fede.stulich@gmail.com>
2021-09-28 04:07:35 -0300

    Requisitos del segundo sprint faltantes

--- frontend/smartmoney/src/components/home/PopUpNewExpense/PopUpNewExpense.js
+++ frontend/smartmoney/src/components/home/PopUpNewExpense/PopUpNewExpense.js
@@ -10,11 +10,6 @@ import "react-datepicker/dist/react-datepicker.css";
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
-// const categoryList=[
-//     'one', 'two', 'three'
-//   ];
-//   const defaultOption = categoryList[0];
-
 const PopUpNewExpense = ({closePopUp, state, expenseToEdit}) => {
 
     const isMobileDevice = useMediaQuery({
@@ -22,13 +17,14 @@ const PopUpNewExpense = ({closePopUp, state, expenseToEdit}) => {
     });
 
     const allCategories = localStorage.allCategories.split(',');
-    const [selectedOption, setSelectedOption] = useState(allCategories[0]);
+    const [selectedOption, setSelectedOption] = useState('');
     const [expenseValue, setExpenseValue] = useState('');
     const [title, setTitle] = useState('New expense');
     const [description, setDescription] = useState('');
     const [date, setDate] = useState(new Date());
     const [newExpenseOnlyNumbers, setNewExpenseOnlyNumbers] = useState(false);
     const [descriptionEmpty, setDescriptionEmpty] = useState(false);
+    const [categoryEmpty, setCategoryEmpty] = useState(false);
     const [newExpenseEmpty, setNewExpenseEmpty] = useState(false);
 
     const loadEditFiles = () =>{
@@ -51,7 +47,8 @@ const PopUpNewExpense = ({closePopUp, state, expenseToEdit}) => {
     const submitNewExpense = () =>{
         const onlyNumbers = /^[0-9]*$/;
         if(description==='')setDescriptionEmpty(true);
-        if(onlyNumbers.test(expenseValue) && description!==''){
+        if(selectedOption==='' )setCategoryEmpty(true);
+        if(onlyNumbers.test(expenseValue) && description!=='' && selectedOption!==''){
           const session = JSON.parse(localStorage.session);
           const requestOptionsNewExpense = {
             method: 'POST',
@@ -64,6 +61,7 @@ const PopUpNewExpense = ({closePopUp, state, expenseToEdit}) => {
                 date: date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate(),
             })
           };
+          console.log(requestOptionsNewExpense)
           fetch('https://smart-money-back.herokuapp.com/new_expense/'+session.user_id+'/', requestOptionsNewExpense)
             .then((response) => {
               if(response.status===201){
@@ -100,6 +98,7 @@ const PopUpNewExpense = ({closePopUp, state, expenseToEdit}) => {
               date: date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate(),
           })
         };
+        console.log(requestOptionsNewExpense.body)
         fetch('https://smart-money-back.herokuapp.com/edit_expense/'+session.user_id+'/', requestOptionsNewExpense)
           .then((response) => {
             if(response.status===200){
@@ -124,14 +123,15 @@ const PopUpNewExpense = ({closePopUp, state, expenseToEdit}) => {
                 <p className="popUpTitle">{title}</p>
                 <form className="formNewExpense">
                     <p className="label">Value</p>
-                    <input style={isMobileDevice ? (newExpenseEmpty ? mobilStyles.inputEmpty : mobilStyles.input) : (newExpenseEmpty ? webStyles.inputEmpty : webStyles.input)} type="text" value={expenseValue} onChange={e => setExpenseValue(e.target.value)} onFocus={()=>setNewExpenseOnlyNumbers(false)} onBlur={()=>isEmpty(expenseValue,setNewExpenseEmpty)}/>
+                    <input style={isMobileDevice ? (newExpenseEmpty ? mobilStyles.inputEmpty : mobilStyles.input) : (newExpenseEmpty ? webStyles.inputEmpty : webStyles.input)} type="text" value={expenseValue} onChange={e => setExpenseValue(e.target.value)} onFocus={()=>{setNewExpenseOnlyNumbers(false);setNewExpenseEmpty(false)}} onBlur={()=>isEmpty(expenseValue,setNewExpenseEmpty)}/>
                     {newExpenseOnlyNumbers&&<p className = "invalidCredentials">Only numbers</p>}
                     {newExpenseEmpty&&<RequiredField/>}
                     <p className="label">Description</p>
                     <input style={isMobileDevice ? (descriptionEmpty ? mobilStyles.inputEmpty : mobilStyles.input) : (descriptionEmpty ? webStyles.inputEmpty : webStyles.input)} type="text" value={description} onChange={e => setDescription(e.target.value)}  onFocus={()=>setDescriptionEmpty(false)} onBlur={()=>isEmpty(description,setDescriptionEmpty)}/>
                     {descriptionEmpty&&<RequiredField/>}
                     <p className="label">Category</p>
-                    <Dropdown options={allCategories} value={selectedOption} onChange={value=>{setSelectedOption(value)}}/>
+                    <Dropdown options={allCategories} value={selectedOption} onChange={value=>{setSelectedOption(value);setCategoryEmpty(false)}}/>
+                    {categoryEmpty&&<RequiredField/>}
                     <p className="label">Date</p>
                     <DatePicker selected={date} onChange={(date) => setDate(date)} />
                     <input 
