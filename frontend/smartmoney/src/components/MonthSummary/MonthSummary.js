959411c6b2d6f7cbb20eb7af1662e514524703a3
Fs <fede.stulich@gmail.com>
2021-10-07 02:47:00 -0300

    month summary

--- frontend/smartmoney/src/components/MonthSummary/MonthSummary.js
+++ frontend/smartmoney/src/components/MonthSummary/MonthSummary.js
@@ -6,27 +6,57 @@ import { IoChevronBackSharp } from "@react-icons/all-files/io5/IoChevronBackShar
 import { IoChevronForwardSharp } from "@react-icons/all-files/io5/IoChevronForwardSharp"; 
 import { style } from "@mui/system";
 import icons from "../../functions/icons";
+import { Pie } from 'react-chartjs-2';
+// import {
+//     Chart,
+//     ChartLegend,
+//     ChartSeries,
+//     ChartSeriesItem,
+//     ChartTitle,
+//     ChartSeriesLabels,
+//   } from "@progress/kendo-react-charts";
 
-import {
-    Chart,
-    ChartLegend,
-    ChartSeries,
-    ChartSeriesItem,
-    ChartTitle,
-    ChartSeriesLabels,
-  } from "@progress/kendo-react-charts";
-
-const colors = ["#141367","#5F1A37","#5E4474","#867A09","#18775C","#7775E6","#F7EC8D","#E59FBC",]
+const colors = ['rgba(255, 99, 132, 1)',
+'rgba(54, 162, 235, 1)',
+'rgba(255, 206, 86, 1)',
+'rgba(75, 192, 192, 1)',
+'rgba(153, 102, 255, 1)',
+'rgba(255, 159, 64, 1)']
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
+const dataFrameAux = {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+          'rgba(255, 159, 64, 0.6)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
 const MonthSummary = ({openPopUpCategoryDetails, update}) => {
 
     const [date, setDate] = useState(new Date());
     const [updatte, setUpdate] = useState(new Date());
     const [categories, setCategories] = useState([]);
-    const [chartCategories,setChartCategories] = useState([]);
+    const [chartCategories,setChartCategories] = useState(dataFrameAux);
     const [chartTotalValue,setChartTotalValue] = useState(0);
     const [chartSubTotalValue,setChartSubTotalValue] = useState(0);
     const [subTotalVisible,setSubTotalVisible] = useState(false);
@@ -35,13 +65,14 @@ const MonthSummary = ({openPopUpCategoryDetails, update}) => {
         const requestOptions = {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ code: session.code})
+          body: JSON.stringify({ code: session.code, month:date.getMonth()+1})
         };
         fetch('https://smart-money-back.herokuapp.com/categories/'+session.user_id+'/', requestOptions)
           .then(response => response.json())
           .then(data => {
-              let categoriesSelected = [];
-              let categoriesChart = [];
+              let categoriesFetch = [];
+              let categoriesChartName = [];
+              let categoriesChartValue = [];
               let chartTotal = 0;
               //data.map( category => {chartTotal = chartTotal + category.total;})
               let i = 0;
@@ -52,18 +83,22 @@ const MonthSummary = ({openPopUpCategoryDetails, update}) => {
                     category.color = colors[i];
                     i++;
                     console.log(category)
-                    categoriesChart.push(category);
+                    categoriesChartName.push(category.name);
+                    categoriesChartValue.push(category.total);
                     chartTotal = chartTotal + category.total
                   }else{
                     category.color = "#AAAAAA";
                   }
-                  categoriesSelected.push(category);
+                  categoriesFetch.push(category);
                 });
               setChartTotalValue(chartTotal);
               setChartSubTotalValue(chartTotal);
-              setChartCategories(categoriesChart);
-              setCategories(categoriesSelected);
-              data.map( category => {})
+              let dataFrame = {...dataFrameAux};
+              dataFrame.labels = categoriesChartName;
+              dataFrame.datasets[0].data = categoriesChartValue;
+              setChartCategories(dataFrame);
+              setCategories(categoriesFetch);
+              console.log(dataFrame)
               //#TODO: SACAR LAS SIGUIETNES 3 LINEAS, CAMBIAR POR RUTA CORRESPONDIENTE
               let allCategories = [];
               data.map( obj => {allCategories.push(obj.name)});
@@ -71,31 +106,32 @@ const MonthSummary = ({openPopUpCategoryDetails, update}) => {
             });
     }
 
-    useEffect(() => fetchCategories(),[update])
+    useEffect(() => fetchCategories(),[update,updatte])
 
     const changeSelectedCategories = (category, add) => {
+        let index = chartCategories.labels.findIndex(x => x ===category.name);
         if(add){
             if(chartTotalValue===(chartSubTotalValue+ category.total))setSubTotalVisible(false);
-            chartCategories.push(category);
+            chartCategories.datasets[0].data[index]=category.total;
+
             setChartSubTotalValue(chartSubTotalValue + category.total);
+            setChartCategories({...chartCategories});
         }else{
-            let index = chartCategories.findIndex(x => x ===category);
-            chartCategories.splice(index,1);
+            chartCategories.datasets[0].data[index]=0;
             setChartSubTotalValue(chartSubTotalValue - category.total)
             setSubTotalVisible(true);
+            setChartCategories({...chartCategories});
         }
     }
-
-    const labelContent = category => icons(category.icon);
 
     return(
         <div className="cardContainer cardContainerMonthSummary">
             <div className="monthSummaryFirstDiv">
-                <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between', alignItems:'start'}}>
+                <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between', alignItems:'start', height:120}}>
                     <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
                         <IoChevronBackSharp onClick={()=>{date.setMonth(date.getMonth() - 1);setUpdate(!updatte)}}/>
                         <p className="monthYearText">{monthNames[date.getMonth()]}, {date.getFullYear()}</p>
-                        <IoChevronForwardSharp onClick={()=>{date.setMonth(date.getMonth() + 1);setUpdate(!updatte)}}/>
+                        <IoChevronForwardSharp onClick={()=>{if(date.getMonth()!==(new Date().getMonth())){date.setMonth(date.getMonth() + 1);setUpdate(!updatte)}}}/>
                     </div>
                     <div>
                         {!subTotalVisible&&<p className="totalText">Total: ${chartTotalValue}</p>}
@@ -104,27 +140,7 @@ const MonthSummary = ({openPopUpCategoryDetails, update}) => {
                     </div>
                 </div>
                 <div className="pieChartContainer">
-                    <Chart style={{height:'100%'}}>
-                        <ChartLegend visible={false} />
-                        <ChartSeries height={500} width={500}>
-                            <ChartSeriesItem
-                                type="pie"
-                                data={chartCategories}
-                                field="total"
-                                categoryField="name"
-                                color= "color"
-                                labels={{
-                                visible: true,
-                                content: "name",
-                                }}
-                            />
-                            {/* <ChartSeriesLabels
-                                color="#fff"
-                                background="none"
-                                content={labelContent}
-                            /> */}
-                        </ChartSeries>
-                    </Chart>
+                    <Pie options= {{maintainAspectRatio: false,plugins: {legend: {display: false}}}} data={chartCategories} />
                 </div>
 
 
