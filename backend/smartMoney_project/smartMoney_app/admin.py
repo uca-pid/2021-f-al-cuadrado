e109ca3fe69d31a3800639dfd745b2f15a5d0150
frandegrandis <frandegrandis@gmail.com>
2021-10-20 01:58:01 -0300

    Bugs fixed

--- backend/smartMoney_project/smartMoney_app/admin.py
+++ backend/smartMoney_project/smartMoney_app/admin.py
@@ -3,7 +3,7 @@ from .category_model import Category
 from .expense_model import Expense
 from .user_model import Sm_user
 from .securityCode_model import SecurityCode
-from .budget_model import Budget
+from .budget_model import Budget,Budget_Category
 
 # Register your models here.
 
@@ -12,3 +12,4 @@ admin.site.register(Expense)
 admin.site.register(Sm_user)
 admin.site.register(SecurityCode)
 admin.site.register(Budget)
+admin.site.register(Budget_Category)
