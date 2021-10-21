d0a6dc065795aa9826c859f1e0e904011c208306
frandegrandis <frandegrandis@gmail.com>
2021-10-21 03:17:54 -0300

    bugFixed

--- backend/smartMoney_project/smartMoney_app/budget_model.py
+++ backend/smartMoney_project/smartMoney_app/budget_model.py
@@ -90,12 +90,12 @@ class Budget(models.Model,baseModel):
 	@classmethod
 	def getBudgetsFrom(cls,from_date,user):
 		date_filter = Q(budget__month__gt = from_date)
-		return Budget_Category.objects.all().filter(date_filter).values('budget__month').annotate(total_budget = Coalesce(models.Sum('total',filter = Q(budget__user = user)),0.0)).order_by('budget__month')
+		return Budget_Category.objects.all().filter(date_filter).filter(Q(budget__user = user)).values('budget__month').annotate(total_budget = Coalesce(models.Sum('total',filter = Q(budget__user = user)),0.0)).order_by('budget__month')
 	
 	@classmethod
 	def getBudgetsOfPeriod(cls,from_date,up_to_date,user):
 		date_filter = Q(budget__month__gte = from_date) & Q(budget__month__lte = up_to_date)
-		return Budget_Category.objects.all().filter(date_filter).values('budget__month').annotate(total_budget = Coalesce(models.Sum('total',filter = Q(budget__user = user)),0.0)).order_by('budget__month')
+		return Budget_Category.objects.all().filter(date_filter).filter(Q(budget__user = user)).values('budget__month').annotate(total_budget = Coalesce(models.Sum('total',filter = Q(budget__user = user)),0.0)).order_by('budget__month')
 
 
 
